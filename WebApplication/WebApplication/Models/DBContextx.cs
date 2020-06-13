720c389036e7a569c771deef4a8ed8ecaf31289a
minhandev <minhan.dev@gmail.com>
2020-06-13 17:40:05 +0700

    .

---
+++ WebApplication/WebApplication/Models/DBContextx.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class DBContextx : DbContext
+    {
+        public DBContextx(DbContextOptions<DBContextx> options) : base(options)
+        {
+
+        }
+        public DbSet<PhieuXuat> PhieuXuats { get; set; }
+    }
+}
